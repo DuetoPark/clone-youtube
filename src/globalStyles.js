7822f85754b8843c740e0af3c100821aab615a96
DuetoPark <inhye.park.dev@gmail.com>
2022-03-22 22:17:10 +0900

    💄 Create globalStyles

---
+++ src/globalStyles.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+import { visuallyHidden } from './styles/utils/common';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  strong {
+    font-weight: 400;
+  }
+
+  ol,
+  ul,
+  li {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    font-family: 'Roboto', sans-serif;
+    border: 0;
+    cursor: pointer;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  .visually-hidden {
+    ${visuallyHidden()}
+  }
+`;
