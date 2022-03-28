a06d160b13aa363ec3988591d17c53c43642b3a4
DuetoPark <inhye.park.dev@gmail.com>
2022-03-28 22:50:17 +0900

    🚧 Add StyledAvatars of Avatar component

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,17 +1,52 @@
 import { StyledAvatar } from './styles';
+import { Link } from 'react-router-dom';
 
-const Avatar = ({ video, size }) => {
+const Avatar = ({ video, size, address }) => {
+  if (address) {
+    if (video.avatarURL) {
+      return (
+        <StyledAvatar
+          as={Link}
+          to={address}
+          className="avatar"
+          size={size}
+          title={video.snippet.channelTitle}
+        >
+          <img src={video.avatarURL} alt={video.snippet.channelTitle} />
+        </StyledAvatar>
+      );
+    } else {
+      return (
+        <StyledAvatar
+          as={Link}
+          to={address}
+          className="avatar"
+          size={size}
+          title={video.snippet.channelTitle}
+        ></StyledAvatar>
+      );
+    }
+  }
+
+  if (video.avatarURL) {
+    return (
+      <StyledAvatar
+        className="avatar"
+        type="button"
+        size={size}
+        title={video.snippet.channelTitle}
+      >
+        <img src={video.avatarURL} alt={video.snippet.channelTitle} />
+      </StyledAvatar>
+    );
+  }
   return (
     <StyledAvatar
-      href="./index.html"
       className="avatar"
+      type="button"
       size={size}
       title={video.snippet.channelTitle}
-    >
-      {video.channelUrl && (
-        <img src={video.channelUrl} alt={video.snippet.channelTitle} />
-      )}
-    </StyledAvatar>
+    ></StyledAvatar>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles';
@@ -9,10 +10,12 @@ import Routes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
