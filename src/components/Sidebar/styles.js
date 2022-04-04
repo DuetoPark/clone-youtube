d7209d54d52abb8e986116869850b0dbc93bdf15
DuetoPark <inhye.park.dev@gmail.com>
2022-04-04 21:41:38 +0900

    💄 Implement Sidebar component

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,166 @@
+import styled from 'styled-components';
+
+import { flexbox, textStyle } from '../../styles/utils';
+
+const SIDEBAR_HEADER_HEIGHT = '56px';
+
+export const StyledSidebar = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: ${({ theme }) => theme.levels.sidebar};
+  width: 240px;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.white};
+  opacity: 0;
+  visibility: hidden;
+  transform: translateX(-100%);
+  transition: transform 200ms ease-in-out, opacity 200ms ease-in-out,
+    visibility 200ms ease-in-out;
+
+  &.is-active {
+    opacity: 1;
+    visibility: visible;
+    transform: translateX(0);
+  }
+
+  .sidebar-header {
+    ${flexbox('start')};
+    height: ${SIDEBAR_HEADER_HEIGHT};
+    padding-left: 16px;
+
+    .logo {
+      padding: 18px 14px 18px 8px;
+      margin-left: 8px;
+
+      svg {
+        display: block;
+      }
+    }
+  }
+
+  .sidebar-content {
+    height: calc(100% - ${SIDEBAR_HEADER_HEIGHT});
+    overflow: scroll;
+  }
+`;
+
+export const StyledSidebarNav = styled.nav`
+  padding-right: 16px;
+  width: 100%;
+
+  > div {
+    padding: 8px 0;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.borderGeneral};
+
+    .menu-title {
+      ${textStyle('md')};
+      padding: 8px 24px;
+      font-weight: 500;
+      color: ${({ theme }) => theme.colors.secondary};
+    }
+
+    &.is-common {
+      padding: 0;
+
+      ul:first-child {
+        padding: 12px 0;
+        border-bottom: 1px solid ${({ theme }) => theme.colors.borderGeneral};
+      }
+
+      ul:last-child {
+        padding: 8px 0;
+      }
+    }
+
+    &.is-desc {
+      ${textStyle('md')};
+      padding: 16px 32px;
+      color: ${({ theme }) => theme.colors.primary};
+
+      p {
+        margin-bottom: 12px;
+      }
+
+      .auth-button {
+        ${flexbox()}
+        height: 36px;
+        padding: 0 15px;
+        border-radius: 2px;
+        border: 1px solid ${({ theme }) => theme.colors.blue};
+        color: ${({ theme }) => theme.colors.blue};
+        transition: background-color 200ms ease-in-out;
+
+        &:active {
+          background-color: ${({ theme }) => theme.colors.borderGeneral};
+        }
+
+        &:hover {
+          background-color: ${({ theme }) => theme.colors.borderGeneral};
+        }
+
+        .ic-auth {
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+`;
+
+export const StyledSidebarFooter = styled.footer`
+  padding: 16px 24px;
+
+  .guide-link-group {
+    ${textStyle('base')};
+    ${flexbox('start')};
+    flex-wrap: wrap;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.secondary};
+
+    &.is-primary {
+      margin-bottom: 12px;
+    }
+
+    &.is-secondary {
+      margin-bottom: 16px;
+    }
+
+    a {
+      &:not(:last-child) {
+        margin-right: 8px;
+      }
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .info {
+    ${textStyle('sm')};
+    color: ${({ theme }) => theme.colors.tertiary};
+
+    .detail {
+      dt,
+      dd {
+        display: inline;
+      }
+
+      dt {
+        flex-grow: 0;
+        flex-shrink: 0;
+        display: inline;
+
+        margin-right: 2px;
+
+        &:after {
+          content: ':';
+        }
+      }
+
+      dd > a:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
