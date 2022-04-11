8708210313e95da5918475f7c3e43d07505bb120
DuetoPark <inhye.park.dev@gmail.com>
2022-04-11 10:14:19 +0900

    ♻️ Refactor Thumbnail component: Apply usePrint

--- src/components/Thumbnail/index.jsx
+++ src/components/Thumbnail/index.jsx
@@ -1,43 +1,12 @@
 import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
+const Thumbnail = (props) => {
+import { usePrint } from '../../hooks';
+
 import { StyledThumbnail } from './styles';
 
-const Thumbnail = (props) => {
-  function getDuration() {
-    let timeData = props.video.contentDetails.duration
-      .replace('PT', '')
-      .split('');
-    let stack = [];
-
-    let duration = new Array(3).fill(0);
-    const HOUR = 0;
-    const MIN = 1;
-    const SEC = 2;
-
-    for (let item of timeData) {
-      if (item === 'H') {
-        duration[HOUR] = stack.join('');
-        stack = [];
-      } else if (item === 'M') {
-        duration[MIN] = stack.join('');
-        stack = [];
-      } else if (item === 'S') {
-        duration[SEC] = stack.join('');
-      } else {
-        stack.push(item);
-      }
-    }
-
-    duration[MIN] = duration[MIN] ? duration[MIN].padStart(2, '0') : '00';
-    duration[SEC] = duration[SEC] ? duration[SEC].padStart(2, '0') : '00';
-
-    if (duration[HOUR] > 0) {
-      return `${duration[HOUR]}:${duration[MIN]}:${duration[SEC]}`;
-    } else {
-      return `${duration[MIN]}:${duration[SEC]}`;
-    }
-  }
+  const { printDuration } = usePrint();
 
   const getVideoComments = useCallback(() => {
     const { id } = props.video;
@@ -55,7 +24,7 @@ const Thumbnail = (props) => {
         src={props.video.snippet.thumbnails.high.url}
         alt={props.video.snippet.title}
       />
-      <span className="duration">{getDuration()}</span>
+      <span className="duration">{printDuration(video)}</span>
     </StyledThumbnail>
   );
 };
