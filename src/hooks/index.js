9f2a2e1741d53e9c0e1589354ab4349a2495a2fa
DuetoPark <inhye.park.dev@gmail.com>
2022-04-12 12:04:02 +0900

    🚧 Create a custom hook: useLocalStorage

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { useResponsive } from './useResponsive';
 export { usePathname } from './usePathname';
 export { usePrint } from './usePrint';
+export { useLocalStorage } from './useLocalStorage';
